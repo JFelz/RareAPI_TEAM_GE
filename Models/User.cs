f57cdac0ea76f23d723bd547c2ee80becd870d5d
ahamm <ahammer753@gmail.com>
2023-08-07 19:50:33 -0500

    Added user data and collection

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TEAMGE_API.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,61 @@
+using TEAMGE_API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+List<User> users = new()
+{
+ new User()
+ {
+    FirstName = "John",
+    LastName = "Doe",
+    Email = "john@example.com",
+    Bio = "Hello, I'm John.",
+    UserName = "johndoe",
+    Password = "password123",
+    ImageUrl = "profile1.jpg"
+ },
+ new User()
+ {
+    FirstName = "Jane",
+    LastName = "Smith",
+    Email = "jane@example.com",
+    Bio = "Nice to meet you!",
+    UserName = "janesmith",
+    Password = "securepwd",
+    ImageUrl = "profile2.jpg"
+ },
+ new User()
+ {
+    FirstName = "Michael",
+    LastName = "Johnson",
+    Email = "michael@example.com",
+    Bio = "Software developer.",
+    UserName = "michaelj",
+    Password = "123456",
+    ImageUrl = "profile3.jpg"
+ },
+ new User()
+ {
+    FirstName = "Emily",
+    LastName = "Brown",
+    Email = "emily@example.com",
+    Bio = "Nature lover.",
+    UserName = "emilyb",
+    Password = "pass123",
+    ImageUrl = "profile4.jpg"
+ },
+ new User()
+ {
+    FirstName = "David",
+    LastName = "Wilson",
+    Email = "david@example.com",
+    Bio = "Travel enthusiast.",
+    UserName = "davidw",
+    Password = "qwerty",
+    ImageUrl = "profile5.jpg"
+ }
+ };
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
