e06ab93ad59cabc9ade2e3ebff6fe98278d5eec7
StevenAJohnsonJr <sajohhnson187@gmail.com>
2023-08-07 19:56:18 -0500

    Post n Subs classes as well as list for each.

---
+++ Models/Subscriptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TEAMGE_API.Models
+{
+    public class Subscriptions
+    {
+        public int Id { get; set; }
+        public int FollowerId { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
