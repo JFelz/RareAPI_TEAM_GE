c69f0253e1765114d4ccc72dba162a1879707702
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-08-07 22:14:34 -0400

    merged with main

--- Program.cs
+++ Program.cs
@@ -429,12 +429,12 @@ app.MapPost("/categories", (Category category) =>
     category.Id = CategoryList.Max(cat => cat.Id) + 1;
     CategoryList.Add(category);
     return CategoryList;
+});
 
 app.MapGet("/posts/{PostId}/comments", (int PostId) =>
 {
     List<Comment> PostCommentsList = CommentList.Where(c => c.PostId == PostId).ToList();
     return PostCommentsList;
-
 });
 
 app.MapGet("/tags", () =>
